fd61b96b1f568986a4ecaf67b936026b0b433179
Firman Azhar Riyadi <firmanazhar14@gmail.com>
2025-02-13 22:54:00 +0700

    vue-router

---
+++ vue-router/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    
+  </header>
+  <div>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/user/1">User1</RouterLink>
+        <RouterLink to="/user/2">User 2</RouterLink>
+      </nav>
+    </div>
+  <RouterView />
+</template>
---
+++ vue-state-management/src/views/UserProfile.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>This is an UserProfile ID:{{userId}} Page</h1>
+    <button @click="goToUser(10)">Go To Profile View</button>
+    <button @click="goToAdminPage">Go To Admin Page</button>
+  </div>
+</template>
+
+
+<script setup>
+import { onBeforeMount, ref, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+
+const route = useRoute();
+const router = useRouter();
+const userId = ref(0)
+
+onBeforeMount(() => {
+  userId.value = route.params.id    
+})
+
+watch(route, () => {
+  userId.value = route.params.id
+})
+
+
+const goToUser = (id) => {
+    router.push(`/profile-view/${id}`) // Berpindah ke halaman dengan ID berbeda
+}
+
+const goToAdminPage = () => {
+    router.push(`/admin`) // Berpindah ke halaman dengan ID berbeda
+}
+
+
+</script>
